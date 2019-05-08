cbaf3879745d4df4d4a9d665143cc6330eb0dcf4
kkurno <kunakorn.pgm@gmail.com>
2019-05-09 01:47:46 +0700

    test

--- src/app/index.js
+++ src/app/index.js
@@ -2,6 +2,7 @@ const database = require('../database');
 
 const run = () => {
   const db = new database();
+  console.log(db.get(['state']));
 
   const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
